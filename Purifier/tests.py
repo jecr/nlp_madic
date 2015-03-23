285af107eb41175fb8f625d2321d69e232fc62a2
Enquire <jorge.ecro@gmail.com>
2015-03-23 13:23:17 -0600

    Strip URLs, users and hashtags

--- Purifier/tests.py
+++ Purifier/tests.py
@@ -14,14 +14,33 @@ of = open(f+'_output', 'w')
 cosa = 0
 x = 0
 urls = []
+twitpics = []
+hashtags = []
+users = []
+
 for line in myFile:
 	cosa = line.split('\t')
 	if line.find('tweet_id') < 0:
-		if cosa[13] != '\n' or cosa[13] != ' ' or cosa[13] != '\t' or cosa[13] != None:
-			if cosa[13].find('http') > 0:
-				urls.append( re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', cosa[13]) )
-				if len(urls) > 0:
-					for x in range( len(urls) ):
-						cosa[13] = cosa[13].replace( urls[x][0] ,'[url]')
-				urls = []
-			of.write(cosa[13].lower())
+		if cosa[13]:
+			cosa[13] = cosa[13].replace( '\n', '')
+			urls = re.findall( 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', cosa[13] )
+			twitpics = re.findall( '(?:pic.twitter)[^"\' ]+', cosa[13] )
+			hashtags = re.findall( '(?:#)[^"\' ]+', cosa[13] )
+			users = re.findall( '(?:@)[^"\' ]+', cosa[13] )
+			if len(urls) > 0:
+				for x in range( len(urls) ):
+					cosa[13] = cosa[13].replace( urls[x], '[url]')
+			urls = []
+			if len(twitpics) > 0:
+				for x in range( len(twitpics) ):
+					cosa[13] = cosa[13].replace( twitpics[x], '[url]')
+			twitpics = []
+			if len(hashtags) > 0:
+				for x in range( len(hashtags) ):
+					cosa[13] = cosa[13].replace( hashtags[x], '[hashtag]')
+			hashtags = []
+			if len(users) > 0:
+				for x in range( len(users) ):
+					cosa[13] = cosa[13].replace( users[x], '[user]')
+			users = []
+			of.write(cosa[13].lower()+'\n')
